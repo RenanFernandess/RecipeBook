1e6ddd8a6d6c9d8f599154c573cce2a146ade0f8
Renan Fernandes <renzinestuods@gmail.com>
2023-07-20 15:15:58 -0300

    :wrench: chore: Configura o docker-compose; Configura o Prisma com o modelo inical do banco de dados

--- packages/backend/src/server.ts
+++ packages/backend/src/server.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from 'express'
 import 'dotenv/config'
 const app = express()
-const port = process.env.PORT
+const port = process.env.SERVER_PORT
 
 app.use(express.json())
 
