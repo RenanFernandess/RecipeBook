24014d06fb697a0c824d83adb768cfb55d805a43
Renan Fernandes <renzinestuods@gmail.com>
2023-07-26 18:41:20 -0300

    :heavy_plus_sign: build: add and configure mobile project

---
+++ packages/mobile/metro.config.js
@@ -0,0 +1,20 @@
+// Learn more https://docs.expo.io/guides/customizing-metro
+const { getDefaultConfig } = require('expo/metro-config');
+const path = require('path');
+
+/** @type {import('expo/metro-config').MetroConfig} */
+
+const projectRoot = __dirname;
+
+const workspaceRoot = path.resolve(projectRoot, '../..');
+
+const config = getDefaultConfig(projectRoot);
+
+config.watchFolders = [workspaceRoot];
+config.resolver.nodeModulesPaths = [
+  path.resolve(projectRoot, 'node_modules'),
+  path.resolve(workspaceRoot, 'node_modules'),
+];
+config.resolver.disableHierarchicalLookup = true;
+
+module.exports = config;
